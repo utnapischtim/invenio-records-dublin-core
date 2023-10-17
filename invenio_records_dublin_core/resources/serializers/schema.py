d0cbacba13f37a59f1fcc48f3bd586c56f55a4c6
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-10-17 23:28:47 +0200

    serializer: add access_status

--- invenio_records_dublin_core/resources/serializers/schema.py
+++ invenio_records_dublin_core/resources/serializers/schema.py
@@ -8,14 +8,25 @@
 
 """Invenio Dublin Core Record Resource schema."""
 
-from marshmallow import Schema
+from marshmallow import Schema, fields
 from marshmallow_utils.fields import SanitizedUnicode
+
+
+def access_status(obj: dict) -> dict:
+    """Access status."""
+    return {
+        "id": "open",
+        "icon": "unlock",
+        "title_l10n": "Open",
+    }
 
 
 class DublinCoreSchema(Schema):
     """Schema for dumping extra information for the dublin core record."""
 
     id = SanitizedUnicode(data_key="id", attribute="id")
+
+    access_status = fields.Function(access_status)
 
     class Meta:
         """Meta class to accept unknwon fields."""

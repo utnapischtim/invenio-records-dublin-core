491c774d1d0be5c1ccd77c3b9d7e749a53c8803c
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-11-22 15:43:07 +0100

    facets: implement better wording for datamodel
    * the previous wording was technical. now it uses the name_l10n version.

--- invenio_records_global_search/services/facets.py
+++ invenio_records_global_search/services/facets.py
@@ -8,12 +8,24 @@
 
 """Facets."""
 
+from flask import current_app
 from invenio_i18n import gettext as _
 from invenio_records_resources.services.records.facets import TermsFacet
+
+
+def data_model_labels(schemas: list) -> dict:
+    """Create l10n names of the data model labels."""
+    labels = current_app.config["GLOBAL_SEARCH_ORIGINAL_SCHEMAS"]
+    out = {}
+    for schema in schemas:
+        out[schema] = labels.get(schema, {}).get("name_l10n", schema)
+    return out
+
 
 data_model = TermsFacet(
     field="original.schema",
     label=_("DataModel"),
+    value_labels=data_model_labels,
 )
 
 subjects = TermsFacet(

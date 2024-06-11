331e8624df4462c591bfb9a784479227a7e0917c
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-06-11 13:28:18 +0200

    schema: allow empty rights
    * this was a necessary change because rights are not mandatory for
      rdm-records so there could be the situation that there are no rights
      set for rdm-records. same holds also for marc21 records.

--- invenio_records_global_search/services/schemas/metadata.py
+++ invenio_records_global_search/services/schemas/metadata.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023 Graz University of Technology.
+# Copyright (C) 2023-2024 Graz University of Technology.
 #
 # invenio-records-global-search is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -21,7 +21,7 @@ class GlobalSearchMetadataSchema(Schema):
     creators = List(String(required=True))
     identifiers = List(String(required=True))
     relations = List(String())
-    rights = List(String(required=True))
+    rights = List(String())
     dates = List(String())
     subjects = List(String())
     descriptions = List(String())
